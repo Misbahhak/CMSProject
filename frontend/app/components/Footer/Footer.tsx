1ed947be6b6a7e1cfd1c86112cc33b718f0c8f26
Muhammad Misbah ul Haq <88501241+Misbahhak@users.noreply.github.com>
2024-05-14 16:40:52 +0500

    Front-End moved in folder
    front end move in to front end folder bcz i want to add sanity and prisma later for backend

--- frontend/app/components/Footer/Footer.tsx
+++ frontend/app/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const Footer = () => {
     return (
@@ -9,8 +10,8 @@ const Footer = () => {
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link href="/" className="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                            <span className='font-black text-tertiary-dark'>
+                            <Image src="/hotellogo.svg" width={50} height={50} alt="hotelz Logo" />
+                            <span className='font-black text-tertiary-dark ml-3'>
                                 Hotelzz
                             </span>
 
@@ -57,7 +58,7 @@ const Footer = () => {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/profile" className="hover:underline">Misbah Hak™</Link>. All Rights Reserved.
                     </span>
-                    <div className="flex mt-4 sm:justify-center sm:mt-0">
+                    {/* <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <Link href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                                 <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
@@ -88,7 +89,7 @@ const Footer = () => {
                             </svg>
                             <span className="sr-only">Dribbble account</span>
                         </Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </footer>
